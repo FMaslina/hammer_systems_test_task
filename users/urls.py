694834d4194fe2f0e1c2a71b2260da30358ad54f
FMaslina <FMaslina@yandex.ru>
2023-08-20 23:51:46 +0700

    Another update urls

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 from .views import login, get_code, fill_profile_data, profile
 from django.urls import path
 
@@ -6,4 +9,4 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('login/code', get_code, name='login_code'),
     path('login/profile', fill_profile_data, name='fill_profile')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
